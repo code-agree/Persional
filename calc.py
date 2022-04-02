8b9deed954271535c6a9252a4520d9a9afb56ee5
余文俊 <yuwenjun@yuwenjundeMacBook-Air.local>
2022-04-02 21:37:21 +0800

    calculate

--- calc.py
+++ calc.py
@@ -1,37 +1,50 @@
-def calculate(s: str) -> int:
-    def helper(s: list) -> int:
-        stack = []
-        sign = '+'
-        num = 0
-        while len(s) > 0:
-            c = s.pop(0)
-            c = str(c)
-            if c.isdigit():
-                num = 10 * num + int(c)
-            if c == '(':
-                num = helper(s)
-            if (not c.isdigit() and c != ' ') or len(s) == 0:
-                if sign == '+':
-                    stack.append(num)
-                elif sign == '-':
-                    stack.append(-num)
-                elif sign == '*':
-                    stack[-1] = stack[-1] * num
-                elif sign == '/':
-                    # python 除法向 0 取整的写法
-                    stack[-1] = int(stack[-1] / float(num))
-                num = 0
-                sign = c
-            if c == ')':
-                break
-        return sum(stack)
+def calculate(input: str) -> int:
+    # 合法值
+    typeext = ['+', '-', '*', '/', '(', ')']
+    error_num = 0
+    for i in range(len(input)):
+        num_single = input[i]
+        if not num_single.isdigit() and num_single not in typeext:
+            error_num += 1
+    if error_num != 0:
+        return "ERROR"
+    elif error_num == 0:
+        def helper(input: list) -> int:
+            stack = []
+            sign = '+'
+            num = 0
+            while len(input) > 0:
+                c = input.pop(0)
+                c = str(c)
+                if c.isdigit():
+                    num = 10 * num + int(c)
+                if c == '(':
+                    num = helper(input)
+                if (not c.isdigit() and c != ' ') or len(input) == 0:
+                    if sign == '+':
+                        stack.append(num)
+                    elif sign == '-':
+                        stack.append(-num)
+                    elif sign == '*':
+                        stack[-1] = stack[-1] * num
+                    elif sign == '/':
+                        # python 除法向 0 取整的写法
+                        stack[-1] = int(stack[-1] / float(num))
+                    num = 0
+                    sign = c
+                if c == ')':
+                    break
+            sum_res = sum(stack)
+            if sum_res >= 2**31 - 1 or sum_res <= - 2**31 - 1:
+                print("OVERFLOW")
+            else:
+                return sum(stack)
     # 需要把字符串转成列表方便操作
-    return helper(list(s))
+    return helper(list(input))
 
 
-
-
-# res = 3*(4-5/5)-6
-res = "3*(6-3)-3/1"
-# res = [3*(4-5/5)-6]
+res = "(4+3)*5+9-7"
 print(calculate(res))
+
+
+# 计算器，向下取整
